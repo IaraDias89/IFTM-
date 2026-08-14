f681239a2f3118d24e09d551652bc6644fff1544
IaraDias89 <iarads.ah@gmail.com>
2026-08-14 01:24:28 -0300

    Create script.js

--- 2°PeriodoJavaScript/lista01/exercicio04/js/script.js
+++ 2°PeriodoJavaScript/lista01/exercicio04/js/script.js
@@ -1,9 +1,15 @@
-nota1 = parseFloat(prompt("Digite a nota do 1º bimestre:"));
-nota2 = parseFloat(prompt("Digite a nota do 2º bimestre:"));
-soma = nota1 + nota2;
-if (soma >= 60) {
-    alert(`Aprovado! Soma das notas: ${soma}`);
-} else {
-    faltam = 60 - soma;
-    alert(`Reprovado! Faltam ${faltam} pontos para a aprovação`);
+nota1 = prompt("Digite a nota do 1º bimestre:");
+nota2 = prompt("Digite a nota do 2º bimestre:");
+
+if (nota1 != null && nota2 != null) {
+    nota1 = parseFloat(nota1);
+    nota2 = parseFloat(nota2);
+    soma = nota1 + nota2;
+
+    if (soma >= 60) {
+        alert(`Aprovado! Soma das notas: ${soma}`);
+    } else {
+        faltam = 60 - soma;
+        alert(`Reprovado! Faltam ${faltam} pontos para a aprovação`);
+    }
 }
