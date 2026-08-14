ca13c5e022d5faaf35883053fc25602721b69fad
IaraDias89 <iarads.ah@gmail.com>
2026-08-14 01:42:37 -0300

    Add files via upload

---
+++ 2°PeriodoJavaScript/lista01/exercicio07/js/script.js
@@ -0,0 +1,4 @@
+r = prompt("Digite o valor de R (0 a 255):");
+g = prompt("Digite o valor de G (0 a 255):");
+b = prompt("Digite o valor de B (0 a 255):");
+document.write(`<p style='color:rgb(${r},${g},${b})'>Fundamentos de Web Design II</p>`);
