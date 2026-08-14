8630497954783809321bdd0405c3ff1ceb084ec1
IaraDias89 <iarads.ah@gmail.com>
2026-08-14 00:51:43 -0300

    Delete 2°PeriodoJavaScript/lista01/exercicio01/script1.js

--- 2°PeriodoJavaScript/lista01/exercicio01/script1.js
+++
@@ -1,6 +0,0 @@
-primeiroNome = prompt("Digite seu primeiro nome:");
-sobrenome = prompt("Digite seu sobrenome:");
-if (primeiroNome != null && sobrenome != null) {
-    nomeCompleto = primeiroNome + " " + sobrenome;
-    alert(`Seu nome completo é: ${nomeCompleto}`);
-}
